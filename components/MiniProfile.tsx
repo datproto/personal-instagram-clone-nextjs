384d692f6305793a870e48cec8c1f36fe347e075
Dat Proto <contact@datproto.com>
2023-01-21 12:40:30 +0700

    FINISH: Add Session to Header & Miniprofile Sidebar

--- components/MiniProfile.tsx
+++ components/MiniProfile.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import {useSession, signOut} from 'next-auth/react'
 
 function MiniProfile(props) {
+  const {data: session} = useSession()
+
   return (
     <div className={'flex items-center justify-between mt-14 ml-10'}>
-      <img src='https://links.papareact.com/3ke' alt='' className={'h-16 w-16 rounded-full border p-[2px]'}/>
+      <img src={session?.user?.image ? session.user.image : 'https://ui-avatars.com/api/?background=0D8ABC&color=fff'} alt='' className={'h-16 w-16 rounded-full border p-[2px]'}/>
 
       <div className={'flex-1 mx-4'}>
-        <h2 className={'font-bold'}>Dat Proto</h2>
+        <h2 className={'font-bold'}>{session?.user?.name}</h2>
         <h3 className={'text-sm text-gray-400'}>Welcome to Instagram</h3>
       </div>
 
-      <button className={'text-blue-400 text-sm font-semibold'}>
+      <button onClick={() => signOut()} className={'text-blue-400 text-sm font-semibold'}>
         Sign out
       </button>
     </div>
