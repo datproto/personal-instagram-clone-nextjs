3635343c38b09397289d5d5e49d2f05cc6dd488f
Dat Proto <contact@datproto.com>
2023-01-22 17:57:00 +0700

    FIX: p cannot be descendant of p

--- components/Post.tsx
+++ components/Post.tsx
@@ -119,13 +119,13 @@ function Post({id, username, avatar, postTitle, postImg, postContent}: PostType)
       )}
 
       {/* Caption */}
-      <p className={'p-5 truncate'}>
+      <div className={'p-5 truncate'}>
         {likes.length > 0 && (
           <p className={'font-bold mb-1'}>{likes.length} likes</p>
         )}
 
         <span className={'font-bold'}>{username} </span>{postTitle}
-      </p>
+      </div>
 
       {/* Comments */}
       {cmts.length > 0 && (
