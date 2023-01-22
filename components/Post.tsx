df65cbe5ffa8c72a1aff81d9a8873e4cc609eb46
Dat Proto <contact@datproto.com>
2023-01-22 10:14:50 +0700

    FINISH: Like functionality; TODO: Deployment

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 import {
   BookmarkIcon,
@@ -12,9 +12,22 @@ import {
   HeartIcon as HeartSolidIcon
 } from '@heroicons/react/24/solid'
 import {useSession} from 'next-auth/react'
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+  setDoc
+} from '@firebase/firestore'
+import {db} from '@/firebase'
+import Moment from 'react-moment'
 
 interface PostType {
-  id: number
+  id: string
   username: string
   avatar: string
   postTitle: string
@@ -24,12 +37,61 @@ interface PostType {
 
 function Post({id, username, avatar, postTitle, postImg, postContent}: PostType) {
   const {data: session} = useSession()
+  const [cmt, setCmt] = useState('')
+  const [cmts, setCmts] = useState([])
+  const [likes, setLikes] = useState([])
+  const [hasLiked, setHasLiked] = useState(false)
+
+  useEffect(() => onSnapshot(
+    query(
+      collection(
+        db, 'posts', id, 'comments'),
+      orderBy('timestamp', 'desc')),
+      snapshot => setCmts((snapshot.docs)
+      )
+    ), [db, id])
+
+  useEffect(() => onSnapshot(
+    collection(db, 'posts', id, 'likes'), snapshot => setLikes(snapshot.docs)
+  ), [db, id])
+
+  useEffect(() => {
+    setHasLiked(
+      likes.findIndex(like => like.id === session?.user?.uid) !== -1
+    )
+  }, [likes])
+
+  const likePost = async() => {
+    if (hasLiked) {
+      await deleteDoc(doc(db, 'posts', id, 'likes', session.user.uid))
+    } else {
+      if ('user' in session) {
+        await setDoc(doc(db, 'posts', id, 'likes', session.user.uid), {
+          username: session.user.username
+        })
+      }
+    }
+  }
+
+  const sentCmt = async (e) => {
+    e.preventDefault()
+
+    const cmtToSend = cmt
+    setCmt('')
+
+    await addDoc(collection(db, 'posts', id, 'comments'), {
+      comment: cmtToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp()
+    })
+  }
 
   return (
     <div className={'bg-white my-7 border rounded-sm'}>
       {/*Header*/}
       <div className={'flex items-center p-5'}>
-        <img src={avatar} alt='avatar' className={'rounded-full h-12 w-12 border p-1 mr-3'}/>
+        <img src={avatar} alt="avatar" className={'rounded-full h-12 w-12 border p-1 mr-3'}/>
         <p className={'flex-1 font-bold'}>{username}</p>
 
         <EllipsisHorizontalIcon className={'h-5'}/>
@@ -40,13 +102,19 @@ function Post({id, username, avatar, postTitle, postImg, postContent}: PostType)
 
       {session && (
         <div className={'flex justify-between px-4 pt-4'}>
-        <div className={'flex space-x-4'}>
-        <HeartIcon className={'btn'} />
-        <ChatBubbleLeftIcon className={'btn'} />
-        <PaperAirplaneIcon className={'btn'} />
-        </div>
+          <div className={'flex space-x-4'}>
+            {
+              hasLiked ? (
+                <HeartSolidIcon onClick={likePost} className={'btn text-red-500'}/>
+              ) : (
+                <HeartIcon onClick={likePost} className={'btn'}/>
+              )
+            }
+            <ChatBubbleLeftIcon className={'btn'}/>
+            <PaperAirplaneIcon className={'btn'}/>
+          </div>
 
-        <BookmarkIcon className={'btn'} />
+          <BookmarkIcon className={'btn'}/>
         </div>
       )}
 
@@ -56,13 +124,32 @@ function Post({id, username, avatar, postTitle, postImg, postContent}: PostType)
       </p>
 
       {/* Comments */}
+      {cmts.length > 0 && (
+        <div className={'ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin'}>
+          {cmts.map(c => (
+            <div key={c.id} className={'flex items-center space-x-2 mb-3'}>
+              <img src={c.data().userImage} alt='' className={'h-7 rounded-full'}/>
+              <p className={'text-sm flex-1'}>
+                <span className={'font-bold'}>{c.data().username}</span>{' '}{c.data().comment}
+              </p>
+
+              <Moment fromNow className={'text-xs pr-5'}>
+                {c.data().timestamp && c.data().timestamp.toDate()}
+              </Moment>
+            </div>
+          ))}
+        </div>
+      )}
 
       {/* Input Box */}
       {session && (
         <form className={'flex items-center p-4'}>
-          <FaceSmileIcon className={'h-7'} />
-          <input type="text" placeholder={'Add a comment ...'} className={'border-none flex-1 focus:ring-0'} />
-          <button className={'font-semibold text-blue-400'}>Post</button>
+          <FaceSmileIcon className={'h-7'}/>
+          <input type="text" placeholder={'Add a comment ...'} className={'border-none flex-1 focus:ring-0'} value={cmt}
+                 onChange={e => setCmt(e.target.value)}/>
+          <button type={'submit'} className={'font-semibold text-blue-400'} disabled={!cmt.trim()}
+                  onClick={sentCmt}>Post
+          </button>
         </form>
       )}
 
